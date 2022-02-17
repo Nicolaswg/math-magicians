956a0510da9c7687583de62dddf40d4c08f59c88
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-02-17 11:45:24 -0400

    Adding test for sum minus and times operation

--- src/__test__/operate.test.js
+++ src/__test__/operate.test.js
@@ -3,8 +3,21 @@ import operate from '../logic/operate';
 describe('testing math operation of operate.js file', () => {
   test('testing sum operation', () => {
     const operation = '+';
-    const operated = operate(8, 9, operation);
+    const operated = operate('8', '9', operation);
 
     expect(operated).toBe('17');
+  });
+
+  test('testing minus operation', () => {
+    const operation = '-';
+    const operated = operate('20', '10', operation);
+    expect(operated).toBe('10');
+  });
+
+  test('testing times operation', () => {
+    const operation = 'x';
+    const operated = operate('4', '4', operation);
+
+    expect(operated).toBe('16');
   });
 });
